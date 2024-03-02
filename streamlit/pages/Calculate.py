fd26408cfb44f915bf40d1dbf91cb90150ffb3ad
kotteletfisk <ostekugle@gmail.com>
2024-03-02 17:33:51 +0100

    UPdated RFC model

--- streamlit/pages/Calculate.py
+++ streamlit/pages/Calculate.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import pickle
+from sklearn.preprocessing import LabelEncoder
+
+column_indexes = ['Body Type', 'Diet', 'How Often Shower', 'Heating Energy Source',
+       'Transport', 'Social Activity', 'Monthly Grocery Bill',
+       'Frequency of Traveling by Air', 'Vehicle Monthly Distance Km',
+       'Waste Bag Size', 'Waste Bag Weekly Count',
+       'How Many New Clothes Monthly']
 
 st.set_page_config(page_title="MP4", page_icon="🧊")
 
@@ -25,15 +33,19 @@ df = pd.read_csv("../data/Carbon-Emission.csv")
 df = df.replace(np.nan, 'None')
 
 def get_df_columns(df):
-    
+
+    df = df[column_indexes]
+
     df_numeric = df.select_dtypes(include=['float64', 'int64'])
     df_object = df.select_dtypes(include=['object'])
 
-    return df_numeric.columns[:-1], df_object.columns
+    return df_numeric.columns, df_object.columns
 
 
 def calculate_carbon_emission(df):
     num_cols, obj_cols = get_df_columns(df)
+
+
 
     input_fields = {}
 
@@ -44,7 +56,7 @@ def calculate_carbon_emission(df):
     for col in obj_cols:
         input_fields[col] = st.selectbox(f"Select {col}", df[col].unique())
 
-    # calculate the carbon emission
+    
     return input_fields
 
 
@@ -52,7 +64,44 @@ entered_values = calculate_carbon_emission(df)
 
 submit = st.button(":green[Calculate Carbon Emission]")
 
+def trans_value(category) -> str:
+    dic = {
+        0: 'Low',
+        1: 'Medium',
+        2: 'High',
+        3: 'Very High'
+    }
+    return dic[category]
+    
+
+def get_numeric_df(df) -> pd.DataFrame:
+     # Encode/transform data numeric
+    label_encoder = LabelEncoder()
+
+    # Making copy of df to a new dataframe called: df_numeric 
+    df_numeric = df
+
+    # Gettning all columns that has object type:
+    cate_columns = df.select_dtypes(include=['object']).columns
+
+    for column in cate_columns:
+        df_numeric[column] = label_encoder.fit_transform(df[column])
+
+    return df_numeric
+
 if submit:
     # ofc should actually calculate the carbon emission here
     # for now just print the entered values
-    entered_values
+    loaded_model = pickle.load(open('../forest_model.pkl', 'rb'))
+    df = pd.DataFrame(entered_values, index=[0])
+
+    df = df[column_indexes]
+
+    df_numeric = get_numeric_df(df)
+
+
+
+    # Predicting the carbon emission
+    prediction = loaded_model.predict(df_numeric)
+    value = trans_value(prediction[0])
+    st.write(f"**Your predicted carbon emission is {value}**")
