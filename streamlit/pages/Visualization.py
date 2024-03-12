25ae050391547a76de5a3e1fb898828979175673
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2024-03-12 11:41:59 +0100

    vehicle and transport

--- streamlit/pages/Visualization.py
+++ streamlit/pages/Visualization.py
@@ -78,6 +78,15 @@ st.markdown(
     This is also supported by the scatter plot below, where we can see a clear positive correlation between the vehicle monthly distance km and the carbon emissions.
     """
 )
+st.image("./graphs/frequency_sex.png")
+
+st.markdown(
+    """
+    From the frequency distribution we can see that there are no significant differences between Male and Female, almost a 50/50 split.
+
+    """
+)
+
 st.markdown("""
 Other high correlation factors:
             
@@ -86,11 +95,22 @@ Other high correlation factors:
     - Transport (0.4)
             
     - Frequency of Traveling by Air (0.3)
-            
 Since vehicle types and styles of transport a very highly dependend on the vehicle monthly distance km, we will primarily focus on this factor for further analysis.
 """)
 
 st.image("./graphs/scatter_vehicle_monthly_distance_km_vs_carbon_emission.png")
+
+col1, col2 = st.columns(2)
+
+col1.image("./graphs/frequency_vehicletype.png")
+col2.image("./graphs/frequency_transport.png")
+
+st.markdown(
+    """
+    In the sample data, the majority of people dont own a car (around 60% ), and more then 33% travel by bike/walk. 
+    """
+)
+            
 
 col1, col2 = st.columns(2)
 st.markdown(
