186f334ee0684512467cffde4ad170dc8ba4e42b
kotteletfisk <ostekugle@gmail.com>
2024-03-13 11:05:20 +0100

    Updated main app

--- streamlit/app.py
+++ streamlit/app.py
@@ -49,9 +49,48 @@ st.markdown(
 
     H5 = Time spent on the internet has a significant impact on an individuals carbon emissions     
     
-    ### Check out
-    - [Our research](/Visualization)
+"""
+)
+
+st.markdown("""
+     ## Table of Contents
+    - [Analysis Highlights](/Visualization)
+        Visual representations of our analysis
     - [Carbon Emissions Classifier](/Classify)
+        Predicting carbon emissions based on categories
     - [Neural Network Prediction](/ANN)
+        Predicting carbon emissions with ANN
+""")
+
+st.markdown(
+"""
+    ## Conclusion
+
+
+In conclusion of our research, we can say it is definetely possible to predict carbon emissions from lifestyle factors.
+We found correlating features which has a big impact on the carbon output, transport choices being among the top.
+We have created a simple frontend application with streamlit, where a user can get predictions from our models, by inputting their own lifestyle factors
+
+- **H0**
+This hypothesis is definetely false, since there is clear correlation between lifestyle factors and carbon emissions
+
+- **H1**
+We can conclude that given the same formatted inputs, we can train regression and ANN models to predict an individuals carbon emissions
+with a model accuracy of around 97% to 98%
+
+- **H2**
+According to our discoveries, the most significant factor
+for individual carbon emissions is style of transport (i. e public or private vehicles) and how many kilometers driven in a private vehicle a month. 
+
+- **H3**
+Out of all the factors we have available in our data, many of the factors had very little correlation to the total carbon emission output, and a few had a little correlation, and even fewer had a strong correlation.
+This gives insight in how the different factors weigh more or less in the total output of carbon emissions, which might be useful for interested parts
+
+- **H4**
+We have used 10000 entries of synthetically generated data from research papers, and we have used standard recognized methods of data-analysis for our models, which gives us accuracy of around 97% for predictions. We believe our conclusions accurately represents reality.
+
+- **H5**
+We have found a small correlation of daily internet usage, with total carbon output, but we have been unable to find any clear tendency between people who spend a lot of time on the internet, and people who emit a lot of carbon. We therefore conclude, that theres no significant impact from this factor.
+
 """
 )
